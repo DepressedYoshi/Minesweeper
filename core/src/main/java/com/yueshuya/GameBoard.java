5778b0866ff5e8673bc68676ce3494b9af0e3ccc
DepressedYoshi <jl2718281828459045@gmail.com>
2024-08-23 13:23:37 -0700

    finished project in Markdown instruction still basice prototyle to be improved with art and AI

--- core/src/main/java/com/yueshuya/GameBoard.java
+++ core/src/main/java/com/yueshuya/GameBoard.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class GameBoard {
     private int[][] board; // data structure as described in the notes
@@ -12,7 +11,7 @@ public class GameBoard {
     private int numFlags; // the number of flags that sill have to be placed by the player
     public static final int BOMB = -1; // help with readability
     private GameplayScreen gameplayScreen;
-
+    private ArrayList<Location> bombSite = new ArrayList<>();
     private Texture emptyTile;
     private Texture emptyFloorTile;
     private Texture bombTile;
@@ -25,11 +24,8 @@ public class GameBoard {
     private Texture sixTile;
     private Texture sevenTile;
     private Texture eightTile;
-
     private final int XOFFSET = 300;
     private final int YOFFSET = 550;
-
-
     public GameBoard(GameplayScreen gameplayScreen){
         this.gameplayScreen = gameplayScreen;
         board = new int[16][30];
@@ -37,7 +33,6 @@ public class GameBoard {
         this.numFlags = numBombs;
         init();
     }
-
     public GameBoard(GameplayScreen gameplayScreen, int numRow, int numCols, int numBombs){
         this.gameplayScreen = gameplayScreen;
         board = new int[numRow][numCols];
@@ -45,13 +40,17 @@ public class GameBoard {
         this.numFlags = numBombs;
         init();
     }
-
+    public int getNumBombs() {
+        return numBombs;
+    }
+    public int getNumFlags() {
+        return numFlags;
+    }
     private void init(){
         loadGraphics();
         placeBomb();
         numberBoard();
     }
-
     private void placeBomb(){
         int placed = 0;
         while (placed < numBombs){
@@ -59,13 +58,26 @@ public class GameBoard {
             int colRandom = (int) (Math.random()*board[0].length);
             if (board[rowRandom][colRandom] != BOMB){
                 board[rowRandom][colRandom] = BOMB;
+                bombSite.add(new Location(rowRandom,colRandom));
                 placed++;
             }
 
         }
     }
+    //pre-condition: numFlag is zero
+    public boolean playerWon() {
+        boolean ans = true;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] < 10){
+                    ans = false;
+                    break;
+                }
+            }
+        }
 
-
+        return ans;
+    }
     private void loadGraphics(){
         emptyTile = new Texture("emptyTile.jpg");
         bombTile = new Texture("bomb.jpg");
@@ -101,7 +113,6 @@ public class GameBoard {
         return neighbors;
 
     }
-
     private int countBomb(ArrayList<Location> neibors){
         int count = 0;
         for(Location l : neibors){
@@ -129,17 +140,70 @@ public class GameBoard {
         //todo fix size adjusting error
         int col = (mouse_x-XOFFSET)/25;
         int row =( board.length -1 ) - (YOFFSET - mouse_y) / 25;
-        return new Location(row,col);
+        Location location = new Location(row,col);
+        if (isValid(location)){
+            return location;
+        }else
+            return null;
     }
-
     public void leftClick(int x, int y) {
-        Location target = getTileAt(x,y);
-        if(board[target.getRow()][target.getCol()] < 9 ){
-            board[target.getRow()][target.getCol()] += 10;
+        Location clickLoc = getTileAt(x, y);
+        // If the click is outside the board, do nothing
+        if (clickLoc == null) {return;}
+        int tileValue = board[clickLoc.getRow()][clickLoc.getCol()];
+        // Handle click on a bomb (game over condition can be handled here)
+        if (tileValue == BOMB) {
+            gameOver();
+            return;
+        }
+        // If we uncover an empty floor, initiate the recursive uncover method
+        if (tileValue == 0) {
+            uncoverArea(clickLoc);
+        } else if (tileValue < 9){
+            // Otherwise, just uncover the clicked tile
+            board[clickLoc.getRow()][clickLoc.getCol()] += 10; // Assuming 10 represents uncovered
         }
     }
+    public void rigtClick(int x, int y) {
+        Location target = getTileAt(x,y);
+        if(board[target.getRow()][target.getCol()] < 9 ){
+            board[target.getRow()][target.getCol()] += 20;
+            numFlags --;
+        } else if (board[target.getRow()][target.getCol()] > 18) {
+            board[target.getRow()][target.getCol()] -= 20;
+            numFlags++;
+        }
 
-
+    }
+    //recursive method that "propogates" through the board uncovering an area of empty tiles, and
+//the surrounding numbers
+    private void uncoverArea(Location loc) {
+        int row = loc.getRow();
+        int col = loc.getCol();
+        int tileVal = board[row][col];
+        if (!isValid(loc) || tileVal >= 10) {
+            return;
+        }
+        if (tileVal < 9 && tileVal != BOMB)
+            board[row][col] += 10;
+        if (countBomb(getNeighbors(loc)) > 0) {
+            return;
+        }
+        // Recurse into all neighboring tiles
+        for (Location neighbor : getNeighbors(loc)) {
+            uncoverArea(neighbor);
+        }
+    }
+    public void showAllBombs() {
+        for (Location l : bombSite){
+            board[l.getRow()][l.getCol()] = 9;
+        }
+    }
+    public void gameOver(){
+        gameplayScreen.setGameOver(true);
+        bombSite.clear();
+        showAllBombs();
+    }
     public void draw(SpriteBatch spriteBatch) {
 
         int size = 25;
@@ -151,7 +215,6 @@ public class GameBoard {
             }
         }
     }
-
     private Texture getTileTexture(int value) {
         switch (value) {
             case 11:
@@ -172,12 +235,15 @@ public class GameBoard {
                 return eightTile;
             case 9:
                 return bombTile;
+            case BOMB:
+                return bombTile;
             case 10:
                 return emptyFloorTile;
-            case 19:
-                return flagTile;
             default:
-                return emptyTile;
+                if (value > 18)
+                    return flagTile;
+                else
+                    return emptyTile;
         }
     }
 }
--- core/src/main/java/com/yueshuya/GameplayScreen.java
+++ core/src/main/java/com/yueshuya/GameplayScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -22,14 +23,16 @@ public class GameplayScreen implements Screen {
     //controll how the camera vew the world
     private Viewport viewport;
     private GameBoard gameBoard;
+    private BitmapFont defaultfont  = new BitmapFont();
 
-
+    private boolean gameOver = false;
 
     /*
     runs one time at the very begginging
     all setup should happend here
     ie: load textures, sounds, setup screen, etc
      */
+
     @Override
     public void show() {
         //2D camera
@@ -45,48 +48,74 @@ public class GameplayScreen implements Screen {
         shapeRenderer = new ShapeRenderer();
 
         shapeRenderer.setAutoShapeType(true);
-         gameBoard = new GameBoard(new GameplayScreen());
+         gameBoard = new GameBoard(this);
     }
-
     public void cleaScreen(){
         Gdx.gl.glClearColor(0.5F,0.5F,0.5F,1.0F);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
     }
+
     private void handleMouseClick() {
+        if(gameBoard.getNumFlags() == 0 && gameBoard.playerWon()) {
+            gameOver = true;
+        }
         if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
             gameBoard.leftClick(Gdx.input.getX(), Gdx.input.getY());
-
+        }
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.RIGHT)){
+            gameBoard.rigtClick(Gdx.input.getX(), Gdx.input.getY());
         }
     }
+    private void drawGUI() {
+        if (!gameOver){
+            defaultfont.draw(spriteBatch,"Bombs left to flag: " + gameBoard.getNumFlags(), 100, 700);
+        }else {
 
+            if(gameBoard.playerWon()) {
+                gameBoard.gameOver();
+                defaultfont.draw(spriteBatch, "GREAT JOB, YOU WIN - press spacebar to play again", 100, 700);
+            }
+            else {
+                defaultfont.draw(spriteBatch, "YOU LOSE - press spacebar to play again", 100, 700);
+            }
+        }
+    }
+    private void handleKeyPresses() {
+        if(gameOver && Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            gameBoard = new GameBoard(this);
+            gameOver = false;
+        }
+    }
     /*
     this method runs as fast as it can or to a set fps
     repeatly, constantly looped
     Things include:
         1. process user input
-        2. A.I. or program logic
+        2. A.I. or program logics
         3. Draw all graphics
      */
     @Override
     public void render(float v) {
         cleaScreen();
+        if (!gameOver){
+            handleMouseClick();
+        }
+        handleKeyPresses();
 
-        handleMouseClick();
 
         // all drawing of shapes must go between begin/end
         shapeRenderer.begin();
         shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.GRAY);
-//        shapeRenderer.rect(0,0,100,100);
         shapeRenderer.end();
 
         //all graphics mush be between the begin and end
         spriteBatch.begin();
         gameBoard.draw(spriteBatch);
+        drawGUI();
         spriteBatch.end();
     }
-
     @Override
     public void resize(int i, int i1) {
         viewport.update(i,i1);
@@ -112,5 +141,9 @@ public class GameplayScreen implements Screen {
     public void dispose() {
         shapeRenderer.dispose();
         spriteBatch.dispose();
+    }
+
+    public void setGameOver(boolean b) {
+        this.gameOver = b;
     }
 }
